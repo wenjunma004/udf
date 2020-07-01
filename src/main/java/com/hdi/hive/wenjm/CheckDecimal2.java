ab1b7080f984012cbbb4a0412802412144a83860
wenjunma004 <wenjunma004@gmail.com>
2020-07-01 09:33:29 -0700

    update

--- src/main/java/com/hdi/hive/wenjm/CheckDecimal2.java
+++ src/main/java/com/hdi/hive/wenjm/CheckDecimal2.java
@@ -4,67 +4,78 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.hadoop.hive.common.type.HiveDecimal;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDTF;
+import org.apache.hadoop.hive.serde2.io.HiveDecimalWritable;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
 import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.HiveDecimalObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.typeinfo.DecimalTypeInfo;
 
 public class CheckDecimal2 extends GenericUDTF {
-    private PrimitiveObjectInspector stringOI = null;
-
-    //Defining input argument as string.
+    private PrimitiveObjectInspector argumentOI = null;
+    //Defining input argument as decimal.
     @Override
     public StructObjectInspector initialize(ObjectInspector[] args) throws UDFArgumentException {
         if (args.length != 1) {
-            throw new UDFArgumentException("NameParserGenericUDTF() takes exactly one argument");
+            throw new UDFArgumentException("CheckDecimal2() takes exactly one argument");
         }
 
         if (args[0].getCategory() != ObjectInspector.Category.PRIMITIVE
-                && ((PrimitiveObjectInspector) args[0]).getPrimitiveCategory() != PrimitiveObjectInspector.PrimitiveCategory.STRING) {
-            throw new UDFArgumentException("NameParserGenericUDTF() takes a string as a parameter");
+                && ((PrimitiveObjectInspector) args[0]).getPrimitiveCategory() != PrimitiveObjectInspector.PrimitiveCategory.DECIMAL) {
+            throw new UDFArgumentException("NameParserGenericUDTF() takes a decimal as a parameter");
         }
 
-        // input
-        stringOI = (PrimitiveObjectInspector) args[0];
+        argumentOI = (PrimitiveObjectInspector) args[0];
 
         // output
         List<String> fieldNames = new ArrayList<String>(2);
         List<ObjectInspector> fieldOIs = new ArrayList<ObjectInspector>(2);
-        fieldNames.add("name");
-        fieldNames.add("surname");
+        fieldNames.add("Original");
+        fieldNames.add("Status");
         fieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
         fieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
         return ObjectInspectorFactory.getStandardStructObjectInspector(fieldNames, fieldOIs);
     }
 
-    public ArrayList<Object[]> processInputRecord(String name){
-        ArrayList<Object[]> result = new ArrayList<Object[]>();
-
-        // ignoring null or empty input
-        if (name == null || "".equals(name)) {
-            return result;
-        }
-
-        String[] tokens = name.split("\\s+");
-
-        if (tokens.length == 2){
-            result.add(new Object[] { tokens[0], tokens[1] });
-        }else if (tokens.length == 4 && tokens[1].equals("and")){
-            result.add(new Object[] { tokens[0], tokens[3] });
-            result.add(new Object[] { tokens[2], tokens[3] });
-        }
-
-        return result;
-    }
 
     @Override
-    public void process(Object[] record) throws HiveException {
-        final String name = stringOI.getPrimitiveJavaObject(record[0]).toString();
-        ArrayList<Object[]> results = processInputRecord(name);
+    public void process(Object[] arguments) throws HiveException {
+
+        Object valObject = arguments[0];
+        if (valObject == null) {
+           // handle it later
+        }
+        ArrayList<Object[]> results = new ArrayList<Object[]>();
+        results.add(new Object[] {valObject.toString(), "OK" });
+
+//        HiveDecimalObjectInspector decimalOI =
+//                (HiveDecimalObjectInspector) argumentOI;
+//        HiveDecimalWritable val = decimalOI.getPrimitiveWritableObject(valObject);
+//
+//        // NPE check logic start
+//
+//        HiveDecimal decimalTypeInfo = decimalOI.getPrimitiveJavaObject(valObject);
+//        DecimalTypeInfo decTypeInfo = (DecimalTypeInfo)decimalOI.getTypeInfo();
+//        try {
+////                   int prec = decTypeInfo.precision();
+//            int scale = decTypeInfo.scale();
+//            byte[] decimalBytes = decimalTypeInfo.bigIntegerBytesScaled(scale);
+//        }catch (NullPointerException npe){
+//            npe.printStackTrace();
+//
+//
+//
+//        }
+
+        // NPE check logic end
+
+
 
         Iterator<Object[]> it = results.iterator();
 
